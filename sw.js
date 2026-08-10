14af3aec56796bc28c7bc6974d2dcb54e26fdad8
sixfour3dp <bmballenger@gmail.com>
2026-08-10 16:32:39 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v44-team-cards-roster';
+const CACHE='pocketbullpen-v45-team-color-settings';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
