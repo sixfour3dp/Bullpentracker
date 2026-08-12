3609ea89b503d44ab25c18ab943791e09bae0824
bballenger <bballenger@tandemadvisors.com>
2026-08-11 22:00:10 -0400

    Add sortable team stats and refine reports

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v49-dark-session-report';
+const CACHE='pocketbullpen-v52-back-buttons';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
