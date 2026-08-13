c34729fe60e41df028fb7ed2f815cdc71ee122cf
bballenger <bballenger@tandemadvisors.com>
2026-08-12 21:11:18 -0400

    Preserve team stats table position

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v54-team-averages';
+const CACHE='pocketbullpen-v55-team-stats-scroll';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
