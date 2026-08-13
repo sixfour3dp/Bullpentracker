750d34ffd540d01a8635b078354174cd4bd7338f
bballenger <bballenger@tandemadvisors.com>
2026-08-12 22:19:24 -0400

    Add custom session management and team settings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v55-team-stats-scroll';
+const CACHE='pocketbullpen-v58-arm-side-labels';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
