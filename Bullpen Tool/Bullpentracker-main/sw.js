c960993dac78815048fbe78d057397d3986004a1
Brett Ballenger <bballenger@tandemadvisors.com>
2026-08-14 14:59:25 -0400

    Restore custom sequence swipe rows

--- Bullpen Tool/Bullpentracker-main/sw.js
+++ Bullpen Tool/Bullpentracker-main/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v59-custom-sequence-rows';
+const CACHE='pocketbullpen-v60-custom-sequence-swipe';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
