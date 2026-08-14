dd6ddcd2321261b91e9a8c39625df513bf299f59
Brett Ballenger <bballenger@tandemadvisors.com>
2026-08-14 14:45:06 -0400

    Rebuild custom sequence pitch rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v58-arm-side-labels';
+const CACHE='pocketbullpen-v59-custom-sequence-rows';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
