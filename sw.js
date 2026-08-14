e650ace0429a5bc40f2eaf04304af3dbeb915e5a
Brett Ballenger <bballenger@tandemadvisors.com>
2026-08-14 15:33:47 -0400

    Do not auto-select custom sequence pitch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v61-selected-session-report';
+const CACHE='pocketbullpen-v62-custom-sequence-no-autoselect';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
