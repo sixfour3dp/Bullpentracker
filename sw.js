1c13446f877cdec4b9cbb010ca31a4c53b943952
Brett Ballenger <bballenger@tandemadvisors.com>
2026-09-09 07:21:32 -0400

    Fix standalone team nav startup gap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v70-team-bottom-nav-gap';
+const CACHE='pocketbullpen-v71-team-nav-standalone-bleed';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
