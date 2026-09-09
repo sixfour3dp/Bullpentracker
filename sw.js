02c145e7fdfb2e0791b0f46b9a62a01ddf7b4b18
Brett Ballenger <bballenger@tandemadvisors.com>
2026-09-09 08:13:45 -0400

    Stabilize home screen team nav dock

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pocketbullpen-v72-team-nav-bottom-fill';
+const CACHE='pocketbullpen-v73-stable-team-nav-dock';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./header-heatmap.png','./pocketbullpen-header.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
